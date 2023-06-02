ce44d6e8e14f0197b61f2838aed5c0faeb9afdc6
Natalia Kuś <66189@student.ue.poznan.pl>
2023-06-02 16:12:54 +0200

    znajdź_największy_element_listy

--- solo_rekurencja.py
+++ solo_rekurencja.py
@@ -8,3 +8,18 @@ def suma_listy(lista):
 # - Jeżeli lista jest pusta to zwracamy 0
 # - Jesli jest inaczej to zwracamy pierwszy element listy, dodany do sumy reszty listy
 # - Wywołujemy funkcję rekurencyjnie dla skróconej listy - bez pierwszego elementu
+
+def znajdz_najwiekszy_element_listy(lista):
+    if len(lista) == 1:
+        return lista[0]
+    else:
+        pierwszy_element = lista[0]
+        reszta_listy = lista[1:]
+        najwiekszy_w_reszcie = znajdz_najwiekszy_element_listy(reszta_listy)
+        return max(pierwszy_element, najwiekszy_w_reszcie)
+
+# Proces:
+# - Jeżeli lista ma tylko jeden element, zwracamy ten element.
+# - W przeciwnym razie, dzielimy listę na pierwszy element i resztę listy.
+# - Rekurencyjnie znajdujemy największy element w reszcie listy.
+# - Porównujemy pierwszy element z największym elementem w reszcie i zwracamy większy z nich.
